0c2a6016a0b207d9c45975f41d2955c0bae56500
lei-clearsky <sportzhulei@gmail.com>
2015-11-01 18:30:52 -0500

    Updated tutorials to demonstrate component lifecycle methods

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -19,10 +19,10 @@ var Comment = React.createClass({
   render: function() {
     return (
       <div className="comment">
-        <h2 className="commentAuthor">
+        <h3 className="commentAuthor">
           {this.props.author}
-        </h2>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+        </h3>
+        <span className="commentBody" dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
@@ -45,10 +45,11 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+
   handleCommentSubmit: function(comment) {
-    var comments = this.state.data;
-    var newComments = comments.concat([comment]);
-    this.setState({data: newComments});
+    // var comments = this.state.data;
+    // var newComments = comments.concat([comment]);
+    // this.setState({data: newComments});
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -65,22 +66,44 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+
+  removeCommentBox: function(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('content'));
+  },
+
   getInitialState: function() {
     return {
       data: [],
-      commentsCount: 0
+      headerText: null
     };
   },
-  componentDidMount: function() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+
+  componentWillMount:function(){
+    this.setState({ headerText: 'Latest Comments' });
   },
+
+  componentDidMount: function() {
+    // Load comments from comments.json
+    this.loadCommentsFromServer();
+    // Update comments every 5 seconds
+    this.refreshIntervalId = setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
+  shouldComponentUpdate:function(nextProps, nextState){
+    return true;
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.refreshIntervalId);
+  },
+
   render: function() {
     return (
       <div className="commentBox">
-        <h1>Comments</h1>
-        <CommentList data={this.state.data} commentsCount={this.state.commentsCount} />
+        <h1>{this.state.headerText}</h1>
+        <CommentList data={this.state.data} />
         <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+        <span className="remove" onClick={this.removeCommentBox}>Remove Comment Box</span>
       </div>
     );
   }
@@ -88,50 +111,49 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   getDefaultProps: function() {
-    console.log("inside getDefaultProps method");
-    return {
-      data: []
-    };
+    console.log("inside CommentList getDefaultProps method");
   },
-  getInitialState:function() {
-    console.log("inside getInitialState method");
-    return {
-      commentsIncreasing: false
-    };
-  },
-  componentWillReceiveProps: function(nextProps) {
-    console.log("inside componentWillReceiveProps method");
-    // console.log('nextProps ', nextProps);
-    // console.log('this.props.commentsCount ', this.props.commentsCount);
-    // console.log('nextProps.commentsCount ', nextProps.commentsCount);
 
-    this.setState({
-      commentsIncreasing: nextProps.commentsCount > this.props.commentsCount
-    });
+  getInitialState: function() {
+    console.log("inside CommentList getInitialState method");
+    return null;
   },
-  shouldComponentUpdate:function(){
-    console.log("inside shouldComponentUpdate method");
+
+  componentWillMount:function(){
+    console.log("inside CommentList componentWillMount method");
+  },
+
+  componentDidMount: function() {
+    console.log("inside CommentList componentDidMount method");
+    console.log("-------------------------------------------");
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    console.log("inside CommentList componentWillReceiveProps method");
+  },
+
+  shouldComponentUpdate:function(nextProps, nextState){
+    console.log("inside CommentList shouldComponentUpdate method");
     return true;
   },
+
   componentWillUpdate:function(){
-    console.log("inside componentWillUpdate method");
+    console.log("inside CommentList componentWillUpdate method");
   },
+
   componentDidUpdate:function(){
-    console.log("inside componentDidUpdate method");
+    console.log("inside CommentList componentDidUpdate method");
+    console.log("--------------------------------------------");
   },
-  componentWillMount:function(){
-    console.log("inside componentWillMount method");
+
+  componentWillUnmount: function() {
+    console.log("inside CommentList componentWillUnmount method");
   },
-  componentDidMount:function(){
-    console.log("inside componentDidMount method");
-  },
+
   render: function() {
-    var hasNewComment = this.state.commentsIncreasing;
+    console.log("inside CommentList render method");
     var commentNodes = this.props.data.map(function(comment, index) {
       return (
-        // `key` is a React-specific concept and is not mandatory for the
-        // purpose of this tutorial. if you're curious, see more here:
-        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
         <Comment author={comment.author} key={index}>
           {comment.text}
         </Comment>
@@ -140,7 +162,6 @@ var CommentList = React.createClass({
     return (
       <div className="commentList">
         {commentNodes}
-        {hasNewComment && <h2>New comment added!</h2>}
       </div>
     );
   }
@@ -158,12 +179,14 @@ var CommentForm = React.createClass({
     this.refs.author.value = '';
     this.refs.text.value = '';
   },
+
   render: function() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Your name" ref="author" />
-        <input type="text" placeholder="Say something..." ref="text" />
-        <input type="submit" value="Post" />
+        <h3>Post a Comment</h3>
+        <input className="commentName" type="text" placeholder="Your name" ref="author" />
+        <textarea className="commentText" placeholder="Say something..." ref="text" rows="4" cols="65"></textarea>
+        <input className="submitButton" type="submit" value="POST" />
       </form>
     );
   }
